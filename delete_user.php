933f7c1c753f72ec819b5ab60f3bd56528c08786
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-04 15:52:15 +0100

    :card_file_box: Add inserts for user table

---
+++ delete_user.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+ini_set('default_charset', 'UTF-8');
+
+if ($_SESSION['login'] == true) 
+{
+    include("db_connect.php");
+    $query = "DELETE FROM users WHERE user_id = $_GET[user_id]";
+    mysqli_query($conn, $query);
+
+    mysqli_close($conn);
+
+    header("location: read.php");
+}
+?>
