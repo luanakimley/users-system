085be9157390da18dc95d785a47da84a6fe83521
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-12 20:23:15 +0100

    :recycle: Put user list styles into main.css

--- user/users_list.php
+++ user/users_list.php
@@ -1,4 +1,6 @@
 <?php
+include('../includes/header.php');
+
 include('../authentication/start_session.php');
 
 if ($_SESSION['login'] == true) 
@@ -7,8 +9,6 @@ if ($_SESSION['login'] == true)
   if ($_SESSION['userType'] != "manager") {
     header('Location: ../user/user_profile.php?user_id=' . $_SESSION['userId']);
   }
-
-  // database connection
 
   include("../db_connect.php");
 
@@ -81,28 +81,6 @@ if ($_SESSION['login'] == true)
 
   $result = mysqli_query($conn, $query);
 
-?>
-
-  <style>
-    .table-button {
-      width: 130px;
-    }
-
-    .table-column {
-      text-align: center;
-    }
-
-    .input-group {
-      max-width: 330px;
-      /* Or whatever width you want the input group to be */
-    }
-  </style>
-
-
-  <!DOCTYPE html>
-  <html lang="en">
-  <?php
-  include('../includes/header.php');
   ?>
 
   <div class="d-flex align-items-center flex-column justify-content-center bg-primary w-100 p-4 mb-5">
@@ -111,7 +89,7 @@ if ($_SESSION['login'] == true)
 
   <main class="container mt-4">
 
-    <div class="row">
+    <div class="row mb-4">
       <div class="col-sm-1">
         <form action="../authentication/logout.php" method="post">
           <input class="btn btn-primary" type="submit" value="Sign Out">
@@ -128,21 +106,22 @@ if ($_SESSION['login'] == true)
         </form>
       </div>
 
-    <div class="col-sm-3">
-      <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-        <div class="form-group">
-          <div class="input-group">
-            <select name="operation" id="operation" class="form-control" style="width: 100px;">
-              <option value="">Sort by...</option>
-              <option value="ascending-name">Name A-Z</option>
-              <option value="descending-name">Name Z-A</option>
-              <option value="ascending-email">Email A-Z</option>
-              <option value="descending-email">Email Z-A</option>
-              <option value="ascending-birthday">Birthdate oldest to youngest</option>
-              <option value="descending-birthday">Birthdate youngest to oldest</option>
-            </select>
-            <div class="input-group-append">
-              <input type="submit" name="submit-order" value="Sort" class="btn btn-outline-success my-2 my-sm-0">
+      <div class="col-sm-3">
+        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+          <div class="form-group">
+            <div class="input-group">
+              <select name="operation" id="operation" class="form-control" style="width: 100px;">
+                <option value="">Sort by...</option>
+                <option value="ascending-name">Name A-Z</option>
+                <option value="descending-name">Name Z-A</option>
+                <option value="ascending-email">Email A-Z</option>
+                <option value="descending-email">Email Z-A</option>
+                <option value="ascending-birthday">Birthdate oldest to youngest</option>
+                <option value="descending-birthday">Birthdate youngest to oldest</option>
+              </select>
+              <div class="input-group-append">
+                <input type="submit" name="submit-order" value="Order" class="btn btn-outline-success my-2 my-sm-0">
+              </div>
             </div>
           </div>
         </form>
@@ -191,38 +170,6 @@ if ($_SESSION['login'] == true)
               <th></th>
               <th></th>
             </tr>
-          </thead>
-          <tbody>
-            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-              <tr>
-                <td><?php echo $row["user_id"]; ?></td>
-                <td><?php echo $row["user_name"]; ?></td>
-                <td><?php echo $row["email"]; ?></td>
-                <td><?php echo $row["birth_date"]; ?></td>
-                <td><?php echo $row["user_type"]; ?></td>
-
-                <td class="table-column">
-                  <?php if ($row["is_active"] == 1) { ?>
-                    <a href="activate_user.php?user_id=<?php echo $row["user_id"]; ?>&is_active=0" class="btn btn-danger btn-sm table-button">DISABLE</a>
-                  <?php } else { ?>
-                    <a href="activate_user.php?user_id=<?php echo $row["user_id"]; ?>&is_active=1" class="btn btn-success btn-sm table-button">ENABLE</a>
-                  <?php } ?>
-                </td>
-                  <td class="table-column">
-                    <?php 
-                    if ($row["user_type"] != "manager") {
-                      if ($row["is_active"] == 1) { ?>
-                        <a href="activate_user.php?user_id=<?php echo $row["user_id"]; ?>&is_active=0" class="btn btn-danger btn-sm table-button">DISABLE</a>
-                      <?php } else { ?>
-                        <a href="activate_user.php?user_id=<?php echo $row["user_id"]; ?>&is_active=1" class="btn btn-success btn-sm table-button">ENABLE</a>
-                        <?php } 
-                    }?>
-                  </td>
-
-                <td class="table-column"><a href="user_profile.php?user_id=<?php echo $row["user_id"]; ?>" class="btn btn-primary btn-sm table-button">VIEW</a></td>
-                <td class="table-column"><a href="edit_user_form.php?user_id=<?php echo $row["user_id"]; ?>" class="btn btn-primary btn-success btn-sm table-button">EDIT</a></td>
-                <td class="table-column"><a href="delete_user.php?user_id=<?php echo $row["user_id"]; ?>" class="btn btn-danger btn-sm table-button">DELETE</a></td>
-              </tr>
             </thead>
             <tbody>
               <?php while ($row = mysqli_fetch_assoc($result)) { ?>
@@ -264,15 +211,9 @@ if ($_SESSION['login'] == true)
     <?php
     include('../includes/footer.php');
     ?>
-  </main>
-  </body>
-
-  </html>
 
 <?php
   // close connection
-                    }
-
   mysqli_close($conn);
 } else {
   header('Location: ../authentication/login_form.php');
